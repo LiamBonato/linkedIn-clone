1dca9ccf92833aa47e4cd0fd78d7d6353438dda0
LiamBonato <liambonato2000@gmail.com>
2024-02-17 15:19:22 +1100

    Testing merge

--- linkedin-clone/src/App.js
+++ linkedin-clone/src/App.js
@@ -5,6 +5,13 @@ function App() {
   return (
     <div className="app">
       <h1>Lets build a LinkedIn Clone</h1>
+
+      {/* Header */}
+
+      {/*App Body*/}
+        {/* Siderbar */}
+        {/* Feed */ }
+        {/* Widgets */}
     </div>
   );
 }
